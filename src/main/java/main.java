fc32e73406b9fac58a86bb9b6622e695ba0dc71e
ivatim <ivantimkin92@mail.ru>
2017-10-03 22:48:14 +0300

    Start of project N4

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,21 @@
+import org.hibernate.Session;
+
 public class main {
     public static void main(String args[]){
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
         User user1 = new User();
         UserInfo userInfo1 =new UserInfo();
         user1.setUserInfo(userInfo1);
         user1.setUserName("Ivan");
         user1.getUserInfo().setAge(24);
         user1.getUserInfo().setSex(true);
+
+        session.save(user1);
+
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
     }
 }
